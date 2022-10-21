47e1794a87d87dc432b577dc3f4c13aac68d204a
Yifan Zhang <yfzhang1999@gmail.com>
2022-10-22 00:35:07 +0800

    panic: print stacktrace on linux

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,4 +1,8 @@
 #include "debug.hpp"
+
+#ifdef __linux__
+# include <execinfo.h>
+#endif
 #include <cstdio>
 #include <cstdlib>
 
@@ -9,6 +13,20 @@ void panic_impl(const char* file, int line, const char* msg)
     } else {
         std::fprintf(stderr, "\n! Panicked at %s:%d\n", file, line);
     }
+
+#ifdef __linux__
+    // get backtrace
+    void* bt[10];
+    const int num_bt = backtrace(bt, 10);
+    const auto symbols = backtrace_symbols(bt, num_bt);
+
+    // print backtrace
+    std::fprintf(stderr, "+ stack backtrace:\n");
+    for (int i = 0; i < num_bt; ++i) {
+        std::fprintf(stderr, "  %d: %s\n", i, symbols[i]);
+    }
+#endif
+
     std::abort();
 }
 

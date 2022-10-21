c8808021ef2f1e0f68168ac93400935b5c6515fd
Yifan Zhang <yfzhang1999@gmail.com>
2022-10-21 23:08:29 +0800

    debug: debug_assert

--- include/debug.hpp
+++ include/debug.hpp
@@ -3,6 +3,14 @@
 #define panic()         panic_impl(__FILE__, __LINE__)
 #define panic_with(msg) panic_impl(__FILE__, __LINE__, msg)
 
+#ifdef NDEBUG
+# define debug_assert(cond) ((void)0)
+#else
+# define debug_assert(cond) do {\
+    if (!(cond)) panic_with("assertion failed: " #cond);\
+} while (0)
+#endif
+
 [[noreturn]]
 void panic_impl(const char* file, int line, const char* msg = nullptr);
 

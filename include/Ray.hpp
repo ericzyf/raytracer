2d0e942302c4301432e632e69a86ce71cedbed3a
Yifan Zhang <yfzhang1999@gmail.com>
2022-10-23 13:39:13 +0800

    Ray

---
+++ include/Ray.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <glm/glm.hpp>
+
+namespace rtx {
+
+class Ray
+{
+public:
+    Ray() = default;
+
+    Ray(glm::vec3 origin, glm::vec3 direction)
+        : origin_(origin),
+          direction_(direction)
+    {
+
+    }
+
+    glm::vec3 origin() const
+    {
+        return origin_;
+    }
+
+    glm::vec3 direction() const
+    {
+        return direction_;
+    }
+
+    glm::vec3 at(const float t) const
+    {
+        return origin_ + t * direction_;
+    }
+
+private:
+    glm::vec3 origin_;
+    glm::vec3 direction_;
+};
+
+}  // namespace rtx

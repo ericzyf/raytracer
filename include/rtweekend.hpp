6df5c82c7342ff99d5cbe57c836055c1f2ef3de3
Yifan Zhang <yfzhang1999@gmail.com>
2022-10-27 19:43:12 +0800

    init rtweekend header

---
+++ include/rtweekend.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <limits>
+
+constexpr float infinity = std::numeric_limits<float>::infinity();
+constexpr float pi = 3.1415926535897932385f;
+
+inline float degrees_to_radians(float deg)
+{
+    return deg * pi / 180.0f;
+}
+

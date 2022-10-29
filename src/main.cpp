c69b674fd18abe0882b290dc9cad063773c638e0
Yifan Zhang <yfzhang1999@gmail.com>
2022-10-29 23:39:46 +0800

    multi thread sampling

--- src/main.cpp
+++ src/main.cpp
@@ -35,9 +35,9 @@ glm::vec3 ray_color(const Ray& r, const IHittable& world, int depth)
 int main(int argc, char* argv[])
 {
     // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1280;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    constexpr auto aspect_ratio = 16.0 / 9.0;
+    constexpr int image_width = 1280;
+    constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
     constexpr float inv_gamma = 1.0f / 2.2f;
     constexpr int samples_per_pixel = 100;
     constexpr int max_depth = 50;
@@ -66,46 +66,44 @@ int main(int argc, char* argv[])
     Camera cam;
 
     // Render
-    Pixmap pm(image_width, image_height);
+    struct {
+        float x;
+        float y;
+    } ds[samples_per_pixel];
 
-    float du[samples_per_pixel];
-    float dv[samples_per_pixel];
     for (int i = 0; i < samples_per_pixel; ++i) {
-        du[i] = random_float();
-        dv[i] = random_float();
+        ds[i].x = random_float();
+        ds[i].y = random_float();
     }
 
-    auto render_row = [&](const int row)
+    auto render_samples = [&](std::vector<glm::vec3>& pm, const int si, const int sj)
     {
-        debug_assert(row >= 0);
-        debug_assert(row < pm.height());
+        debug_assert(si >= 0 && si < samples_per_pixel);
+        debug_assert(sj >= 0 && sj < samples_per_pixel);
+        debug_assert(si <= sj);
 
-        auto row_pm = pm.data() + row * pm.width();
-        for (int i = 0; i < image_width; ++i) {
-            auto pixel_color = glm::vec3(0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + du[s]) / (image_width - 1);
-                auto v = (row + dv[s]) / (image_height - 1);
-                auto r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
+        const auto num_samples = sj - si;
+        auto ppm = pm.begin();
 
-            // gamma correction
-            *row_pm++ = RGB(
-                glm::pow(
-                    pixel_color / static_cast<float>(samples_per_pixel),
+        for (int y = 0; y < image_height; ++y) {
+            for (int x = 0; x < image_width; ++x) {
+                auto pixel_color = glm::vec3(0);
+                for (int s = si; s != sj; ++s) {
+                    const auto u = (x + ds[s].x) / (image_width - 1);
+                    const auto v = (y + ds[s].y) / (image_height - 1);
+                    pixel_color += ray_color(
+                        cam.get_ray(u, v),
+                        world,
+                        max_depth
+                    );
+                }
+
+                // average samples and then apply gamma correction
+                *ppm++ = glm::pow(
+                    pixel_color / static_cast<float>(num_samples),
                     glm::vec3(inv_gamma)
-                )
-            );
-        }
-    };
-
-    auto render_row_range = [&](int begin, int end)
-    {
-        debug_assert(begin <= end);
-
-        for (; begin < end; ++begin) {
-            render_row(begin);
+                );
+            }
         }
     };
 
@@ -115,16 +113,40 @@ int main(int argc, char* argv[])
     std::vector<std::thread> workers;
     workers.reserve(num_threads);
 
+    std::vector<std::vector<glm::vec3>> worker_pm(num_threads);
+    for (auto& pm : worker_pm) {
+        pm.resize(image_width * image_height);
+    }
+
     // Dispatch render tasks
-    for (int i = 0, curr_row = 0; i < num_threads; ++i) {
-        const int num_rows = image_height / num_threads + (i < image_height % num_threads);
-        workers.emplace_back(render_row_range, curr_row, curr_row + num_rows);
-        curr_row += num_rows;
+    for (int i = 0, curr_sample = 0; i < num_threads; ++i) {
+        const int num_samples =
+            samples_per_pixel / num_threads + (i < samples_per_pixel % num_threads);
+
+        workers.emplace_back(
+            render_samples,
+            std::ref(worker_pm[i]), curr_sample, curr_sample + num_samples
+        );
+
+        curr_sample += num_samples;
     }
 
     // Join
     for (auto& worker : workers) {
         worker.join();
+    }
+
+    // sum pixmaps
+    for (int i = 1; i < num_threads; ++i) {
+        auto& pm = worker_pm[i];
+        for (size_t j = 0; j < pm.size(); ++j) {
+            worker_pm[0][j] += pm[j];
+        }
+    }
+
+    Pixmap pm(image_width, image_height);
+    for (size_t i = 0, n = image_width * image_height; i < n; ++i) {
+        pm.data()[i] = RGB(worker_pm[0][i] / static_cast<float>(num_threads));
     }
 
     pm.write_bmp("image.bmp");

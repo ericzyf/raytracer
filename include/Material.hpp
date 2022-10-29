3903bf1bc7a1651b237b9739315b5d39907f5ada
Yifan Zhang <yfzhang1999@gmail.com>
2022-10-29 15:34:29 +0800

    9.3 Lambertian

--- include/Material.hpp
+++ include/Material.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "rtweekend.hpp"
 #include "Hittable.hpp"
 
 namespace rtx {
@@ -15,6 +16,41 @@ public:
 
     virtual ~IMaterial() = default;
 };
+
+namespace Material {
+
+class Lambertian: public IMaterial
+{
+public:
+    explicit Lambertian(glm::vec3 a)
+        : albedo_(a)
+    {
+
+    }
+
+    bool scatter(
+        const Ray& r_in,
+        const HitRecord& rec,
+        glm::vec3& attenuation,
+        Ray& scattered
+    ) const override
+    {
+        auto scatter_direction = rec.normal + random_unit_vec3();
+        // Catch degenerate scatter direction
+        if (near_zero_vec3(scatter_direction)) {
+            scatter_direction = rec.normal;
+        }
+
+        scattered = { rec.p, scatter_direction };
+        attenuation = albedo_;
+        return true;
+    }
+
+private:
+    glm::vec3 albedo_;
+};
+
+}  // namespace Material
 
 }  // namespace rtx
 
